eb06385c9d89af2bc996c2035e45171efe3c16da
kyeolee89 <kyeolee89@gmail.com>
2017-08-15 16:27:55 +0900

    SpringBoot Setup

---
+++ src/main/java/sample/SampleController.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/")
+    public String sample() {
+        return "Sample Page";
+    }
+}
---
+++ src/main/java/sample/ServletInitializer.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SampleSpringBootApplication.class);
+    }
+}
